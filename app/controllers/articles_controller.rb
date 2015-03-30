f5a779ae7cb6928dc860068e068129c94ce60cbd
Beatriz Santos <bsantosb@snu.ac.kr>
2015-03-30 20:45:42 +0900

    Practice Session Rails

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def show
+   @article = Article.find(params[:id])
+   #http://localhost:3000/articles/id
+  end
+
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # Initialize new model with filtered parameter.
+    @article.save
+    # Store the model to DB
+    # Returns success or failure as a boolean value
+    redirect_to @article
+    # Redirect automatically to 'show' action, defined later.
+  end
+
+  private 
+  def article_params
+    params.require(:article).permit(:title, :text)
+    # Whitelist our controller parameters to prevent wrongful mass assignment
+  end
+end
