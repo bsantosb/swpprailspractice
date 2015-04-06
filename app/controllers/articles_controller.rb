1b9709ce961c5fbae13e25b95318562635e87108
Beatriz Santos <bsantosb@snu.ac.kr>
2015-04-06 20:37:16 +0900

    Practice Session Rails commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,34 @@
 class ArticlesController < ApplicationController
+
+
+def edit
+  @article = Article.find(params[:id])
+end
+def update
+  @article = Article.find(params[:id])
+ if @article.update(article_params)
+   redirect_to @article
+ else
+  render 'edit'
+ end
+end
+
+
   def show
    @article = Article.find(params[:id])
    #http://localhost:3000/articles/id
   end
 
-
-  def new
-  end
+ def new
+  @article = Article.new
+ end
+ def create
+  @article = Article.new(article_params)
+ if @article.save
+  redirect_to @article # Go to article list
+ else
+  render 'new' # Show the new form again
+ end
 
   def create
     @article = Article.new(article_params)
